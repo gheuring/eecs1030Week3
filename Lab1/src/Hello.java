3c13f1b9345fd70b8fa470207538745a9cdeaa4c
gheuring <79321251+gheuring@users.noreply.github.com>
2023-09-12 10:44:38 -0400

    Add files via upload
    Initial load of repository

---
+++ Lab1/src/Hello.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * Name:  Jerry Heuring 
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+		System.out.println("Goodbye for now...");
+		System.exit(0);
+	}
+
+}
